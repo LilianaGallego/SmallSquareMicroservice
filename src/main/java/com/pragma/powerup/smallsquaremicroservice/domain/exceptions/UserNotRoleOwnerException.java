84ff78c050f0f7d5bfc4a2a010757605d3e3d734
LilianaGallego <lilianagallegom@gmail.com>
2023-05-07 13:04:23 -0500

    UserNotRoleOwnerException is created
    UserNotRoleOwnerException is created to handle exceptions when role is not owner

---
+++ src/main/java/com/pragma/powerup/smallsquaremicroservice/domain/exceptions/UserNotRoleOwnerException.java
@@ -0,0 +1,7 @@
+package com.pragma.powerup.smallsquaremicroservice.domain.exceptions;
+
+public class UserNotRoleOwnerException extends RuntimeException {
+    public UserNotRoleOwnerException() {
+        super();
+    }
+}
