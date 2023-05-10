f367c70d36cd04b12ed5cda8e6304f6dadeceb71
LilianaGallego <lilianagallegom@gmail.com>
2023-05-10 18:56:25 -0500

    -m "id withdrawal" -m "id withdrawal"

--- src/main/java/com/pragma/powerup/smallsquaremicroservice/domain/model/User.java
+++ src/main/java/com/pragma/powerup/smallsquaremicroservice/domain/model/User.java
@@ -1,11 +1,15 @@
 package com.pragma.powerup.smallsquaremicroservice.domain.model;
 
-import java.time.LocalDate;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Getter
+@Setter
 public class User {
 
-    private Long id;
+
     private String name;
     private String surname;
     private String mail;
@@ -17,9 +21,9 @@ public class User {
     public User(){}
 
 
-    public User(Long id, String name, String surname, String mail, String phone, Date birthdate, String dniNumber,
+    public User(String name, String surname, String mail, String phone, Date birthdate, String dniNumber,
                 String password, Long idRole) {
-        this.id = id;
+
         this.name = name;
         this.surname = surname;
         this.mail = mail;
@@ -30,75 +34,5 @@ public class User {
         this.idRole = idRole;
     }
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getMail() {
-        return mail;
-    }
-
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public Date getBirthdate() {
-        return birthdate;
-    }
-
-    public void setBirthdate(Date birthdate) {
-        this.birthdate = birthdate;
-    }
-
-    public String getDniNumber() {
-        return dniNumber;
-    }
-
-    public void setDniNumber(String dniNumber) {
-        this.dniNumber = dniNumber;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Long getIdRole() {
-        return idRole;
-    }
-
-    public void setIdRole(Long idRole) {
-        this.idRole = idRole;
-    }
 }
