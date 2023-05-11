77c2746273b91b53c097cb58bb92d55e265f29e2
LilianaGallego <lilianagallegom@gmail.com>
2023-05-10 19:00:48 -0500

    -m "update Import and implementation RestTemplate" -m "The import and use of the adapter that implements the RestTemplate for the connection with the user microservice is updated"

--- src/main/java/com/pragma/powerup/smallsquaremicroservice/domain/usecase/RestaurantUseCase.java
+++ src/main/java/com/pragma/powerup/smallsquaremicroservice/domain/usecase/RestaurantUseCase.java
@@ -1,20 +1,18 @@
 package com.pragma.powerup.smallsquaremicroservice.domain.usecase;
 
-import com.pragma.powerup.smallsquaremicroservice.adapters.driving.http.controller.OwnerRestController;
+import com.pragma.powerup.smallsquaremicroservice.adapters.driving.http.adapter.OwnerHttpAdapter;
 import com.pragma.powerup.smallsquaremicroservice.configuration.Constants;
-import com.pragma.powerup.smallsquaremicroservice.domain.exceptions.RestaurantNotCreatedException;
 import com.pragma.powerup.smallsquaremicroservice.domain.exceptions.UserNotRoleOwnerException;
 import com.pragma.powerup.smallsquaremicroservice.domain.model.Restaurant;
 import com.pragma.powerup.smallsquaremicroservice.domain.model.User;
 import com.pragma.powerup.smallsquaremicroservice.domain.spi.IRestaurantPersistencePort;
 import com.pragma.powerup.smallsquaremicroservice.domain.api.IRestaurantServicePort;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 
 public class RestaurantUseCase implements IRestaurantServicePort {
     private final IRestaurantPersistencePort restaurantPersistencePort;
     @Autowired
-    private OwnerRestController restController;
+    protected OwnerHttpAdapter ownerHttpAdapter;
 
     public RestaurantUseCase(IRestaurantPersistencePort restaurantPersistencePort) {
         this.restaurantPersistencePort = restaurantPersistencePort;
@@ -23,11 +21,10 @@ public class RestaurantUseCase implements IRestaurantServicePort {
     @Override
     public void saveRestaurant(Restaurant restaurant) {
         Long id = restaurant.getIdOwner();
-        String name;
 
-        User user = restController.createRestaurant(id);
-        System.out.println(user.getIdRole());
-        if (user.getIdRole()!= Constants.OWNER_ROLE_ID){
+        User user = ownerHttpAdapter.getOwner(id);
+
+        if (!user.getIdRole().equals(Constants.OWNER_ROLE_ID) ){
             throw new UserNotRoleOwnerException();
         }
 
