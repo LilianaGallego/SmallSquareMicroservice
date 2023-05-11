1e673d9dfe00920c3134193b338dff8fb9e5fc25
LilianaGallego <lilianagallegom@gmail.com>
2023-05-10 22:04:40 -0500

    refactor: "Constants Update: The constants is added for when the user is searched and the user is not found in the database"

--- src/main/java/com/pragma/powerup/smallsquaremicroservice/configuration/Constants.java
+++ src/main/java/com/pragma/powerup/smallsquaremicroservice/configuration/Constants.java
@@ -6,14 +6,11 @@ public class Constants {
         throw new IllegalStateException("Utility class");
     }
 
-    public static final int MAX_PAGE_SIZE = 2;
     public static final Long OWNER_ROLE_ID = 2L;
-
     public static final String RESPONSE_MESSAGE_KEY = "message";
     public static final String RESTAURANT_CREATED_MESSAGE = "Restaurant created successfully";
-    public static final String RESTAURANT_DELETED_MESSAGE = "Restaurant deleted successfully";
     public static final String RESPONSE_ERROR_MESSAGE_KEY = "error";
-    public static final String WRONG_CREDENTIALS_MESSAGE = "Wrong credentials";
+    public static final String USER_NOT_FOUND_MESSAGE = "No user found with the role provided";
     public static final String NO_DATA_FOUND_MESSAGE = "No data found for the requested petition";
     public static final String RESTAURANT_ALREADY_EXISTS_MESSAGE = "A Restaurant already exists with the role provided";
     public static final String RESTAURANT_NOT_FOUND_MESSAGE = "No Restaurant found with the role provided";
