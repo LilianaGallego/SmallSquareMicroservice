4a2f4ce433288a8cb9aba3f650249ae9848fa224
LilianaGallego <lilianagallegom@gmail.com>
2023-05-12 22:03:51 -0500

    ci: "Update Exceptions: Exceptions for restaurant field validations are updated"

--- src/main/java/com/pragma/powerup/smallsquaremicroservice/adapters/driving/http/dto/response/RestaurantResponseDto.java
+++ src/main/java/com/pragma/powerup/smallsquaremicroservice/adapters/driving/http/dto/response/RestaurantResponseDto.java
@@ -1,6 +1,5 @@
 package com.pragma.powerup.smallsquaremicroservice.adapters.driving.http.dto.response;
 
-import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
