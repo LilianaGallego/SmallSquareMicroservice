368f9cd9d07db29a29cec956360c77e2db222142
LilianaGallego <lilianagallegom@gmail.com>
2023-05-15 11:08:06 -0500

    buil: "Create Exceptions: The exceptions for the category and the dish are created"

---
+++ src/main/java/com/pragma/powerup/smallsquaremicroservice/adapters/driven/jpa/mysql/exceptions/PlateAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.pragma.powerup.smallsquaremicroservice.adapters.driven.jpa.mysql.exceptions;
+
+public class PlateAlreadyExistsException extends RuntimeException {
+    public PlateAlreadyExistsException() {
+        super();
+    }
+}
---
+++ src/main/java/com/pragma/powerup/smallsquaremicroservice/domain/exceptions/CategoryNotExistException.java
@@ -0,0 +1,7 @@
+package com.pragma.powerup.smallsquaremicroservice.domain.exceptions;
+
+public class CategoryNotExistException extends RuntimeException {
+    public CategoryNotExistException() {
+        super();
+    }
+}
