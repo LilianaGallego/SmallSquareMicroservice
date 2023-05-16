4d5d0abde1c9d0ea11de8171764e0c41361ba93d
LilianaGallego <lilianagallegom@gmail.com>
2023-05-15 20:39:16 -0500

    build: "Add mapping for restaurant and category"

--- src/main/java/com/pragma/powerup/smallsquaremicroservice/domain/model/Category.java
+++ src/main/java/com/pragma/powerup/smallsquaremicroservice/domain/model/Category.java
@@ -1,16 +1,41 @@
 package com.pragma.powerup.smallsquaremicroservice.domain.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class Category {
     private Long id;
     private String name;
     private String description;
+
+    public Category() {
+    }
+
+    public Category(Long id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- src/main/java/com/pragma/powerup/smallsquaremicroservice/domain/model/Plate.java
+++ src/main/java/com/pragma/powerup/smallsquaremicroservice/domain/model/Plate.java
@@ -1,14 +1,6 @@
 package com.pragma.powerup.smallsquaremicroservice.domain.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class Plate {
 
     private Long id;
@@ -19,4 +11,83 @@ public class Plate {
     private Category category;
     private Boolean active;
     private Restaurant restaurant;
+
+
+    public Plate() {
+    }
+
+    public Plate(Long id, String name, int price, String description, String urlImage, Category category, Boolean active, Restaurant restaurant) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.urlImage = urlImage;
+        this.category = category;
+        this.active = active;
+        this.restaurant = restaurant;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrlImage() {
+        return urlImage;
+    }
+
+    public void setUrlImage(String urlImage) {
+        this.urlImage = urlImage;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
 }
