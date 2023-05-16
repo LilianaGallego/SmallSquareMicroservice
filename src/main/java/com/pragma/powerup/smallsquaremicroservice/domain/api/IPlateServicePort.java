fd6463f9e643abc20c65d105eda2e9e55009f16b
LilianaGallego <lilianagallegom@gmail.com>
2023-05-15 20:42:04 -0500

    build: "validates for plate: added validates for plate"

--- src/main/java/com/pragma/powerup/smallsquaremicroservice/domain/api/IPlateServicePort.java
+++ src/main/java/com/pragma/powerup/smallsquaremicroservice/domain/api/IPlateServicePort.java
@@ -5,4 +5,10 @@ import com.pragma.powerup.smallsquaremicroservice.domain.model.Plate;
 public interface IPlateServicePort {
 
     void savePlate(Plate plate);
+    void validateName(String name);
+    void validatePrice(int price);
+    void validateRestaurantId(Long restaurantId);
+    void validateCategoryId(Long categoryId);
+    void validateDescription(String description);
+    void validateUrlImage(String urlLogo);
 }
