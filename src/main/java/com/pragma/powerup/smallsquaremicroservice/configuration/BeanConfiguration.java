fd76e851c0731620613ea9592e6e2de79eeb48a1
LilianaGallego <lilianagallegom@gmail.com>
2023-05-15 20:37:37 -0500

    build: "Exceptions fields when creating the plate: Added validations for fields when creating a dish"

--- src/main/java/com/pragma/powerup/smallsquaremicroservice/configuration/BeanConfiguration.java
+++ src/main/java/com/pragma/powerup/smallsquaremicroservice/configuration/BeanConfiguration.java
@@ -1,8 +1,20 @@
 package com.pragma.powerup.smallsquaremicroservice.configuration;
 
+import com.pragma.powerup.smallsquaremicroservice.adapters.driven.jpa.mysql.adapter.CategoryMysqlAdapter;
+import com.pragma.powerup.smallsquaremicroservice.adapters.driven.jpa.mysql.adapter.PlateMysqlAdapter;
+import com.pragma.powerup.smallsquaremicroservice.adapters.driven.jpa.mysql.mappers.ICategoryEntityMapper;
+import com.pragma.powerup.smallsquaremicroservice.adapters.driven.jpa.mysql.mappers.IPlateEntityMapper;
 import com.pragma.powerup.smallsquaremicroservice.adapters.driven.jpa.mysql.mappers.IRestaurantEntityMapper;
+import com.pragma.powerup.smallsquaremicroservice.adapters.driven.jpa.mysql.repositories.ICategoryRepository;
+import com.pragma.powerup.smallsquaremicroservice.adapters.driven.jpa.mysql.repositories.IPlateRepository;
+import com.pragma.powerup.smallsquaremicroservice.domain.api.ICategoryServicePort;
+import com.pragma.powerup.smallsquaremicroservice.domain.api.IPlateServicePort;
 import com.pragma.powerup.smallsquaremicroservice.domain.api.IRestaurantServicePort;
+import com.pragma.powerup.smallsquaremicroservice.domain.spi.ICategoryPersistencePort;
+import com.pragma.powerup.smallsquaremicroservice.domain.spi.IPlatePersistencePort;
 import com.pragma.powerup.smallsquaremicroservice.domain.spi.IRestaurantPersistencePort;
+import com.pragma.powerup.smallsquaremicroservice.domain.usecase.CategoryUseCase;
+import com.pragma.powerup.smallsquaremicroservice.domain.usecase.PlateUseCase;
 import com.pragma.powerup.smallsquaremicroservice.domain.usecase.RestaurantUseCase;
 import com.pragma.powerup.smallsquaremicroservice.adapters.driven.jpa.mysql.adapter.RestaurantMysqlAdapter;
 import com.pragma.powerup.smallsquaremicroservice.adapters.driven.jpa.mysql.repositories.IRestaurantRepository;
@@ -17,6 +29,12 @@ public class BeanConfiguration {
     private final IRestaurantRepository restaurantRepository;
     private final IRestaurantEntityMapper restaurantEntityMapper;
 
+    private final IPlateRepository plateRepository;
+    private final IPlateEntityMapper plateEntityMapper;
+
+    private final ICategoryEntityMapper categoryEntityMapper;
+    private final ICategoryRepository categoryRepository;
+
 
     @Bean
     public IRestaurantServicePort restaurantServicePort() {
@@ -28,7 +46,25 @@ public class BeanConfiguration {
     }
 
     @Bean
+    public IPlateServicePort plateServicePort() {
+        return new PlateUseCase(platePersistencePort(), restaurantRepository, categoryRepository);
+    }
+    @Bean
+    public IPlatePersistencePort platePersistencePort() {
+        return new PlateMysqlAdapter(plateRepository, plateEntityMapper);
+    }
+
+    @Bean
     public RestTemplate restTemplate(){
         return new RestTemplate();
+    }
+
+    @Bean
+    public ICategoryServicePort categoryServicePort() {
+        return new CategoryUseCase(categoryPersistencePort());
+    }
+    @Bean
+    public ICategoryPersistencePort categoryPersistencePort() {
+        return new CategoryMysqlAdapter(categoryRepository, categoryEntityMapper);
     }
 }
