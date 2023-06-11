335ec415c35e556803193fc26993acaa1b7609e2
LilianaGallego <lilianagallegom@gmail.com>
2023-06-10 21:20:10 -0500

    test: "Test make an order"

--- src/test/java/com/pragma/powerup/smallsquaremicroservice/domain/usecase/OrderUseCaseTest.java
+++ src/test/java/com/pragma/powerup/smallsquaremicroservice/domain/usecase/OrderUseCaseTest.java
@@ -1,5 +1,8 @@
 package com.pragma.powerup.smallsquaremicroservice.domain.usecase;
 
+import com.pragma.powerup.smallsquaremicroservice.adapters.driven.jpa.mysql.entity.OrderEntity;
+import com.pragma.powerup.smallsquaremicroservice.adapters.driven.jpa.mysql.entity.RestaurantEntity;
+import com.pragma.powerup.smallsquaremicroservice.adapters.driven.jpa.mysql.exceptions.OrderInProcessesException;
 import com.pragma.powerup.smallsquaremicroservice.configuration.security.TokenInterceptor;
 import com.pragma.powerup.smallsquaremicroservice.domain.api.IOrderServicePort;
 import com.pragma.powerup.smallsquaremicroservice.domain.model.Order;
@@ -17,6 +20,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -33,7 +37,7 @@ class OrderUseCaseTest {
     }
 
     @Test
-    void testSaveOrder() {
+    void testSaveOrderStateCancelled() {
         // Arrange
         Long idRestaurant = 1L;
         Long idClient = 2L;
@@ -44,12 +48,18 @@ class OrderUseCaseTest {
         order.setDate(LocalDate.now());
         order.setState(StateEnum.EARNING);
         order.setRestaurant(restaurant);
+        RestaurantEntity restaurantEntity = new RestaurantEntity();
+        restaurantEntity.setId(idRestaurant);
+        OrderEntity orderEntity = new OrderEntity(1L,idClient,LocalDate.now(),StateEnum.CANCELLED,10L,restaurantEntity);
 
         when(restaurantPersistencePort.findById(idRestaurant)).thenReturn(restaurant);
+        when(orderPersistencePort.existsByIdClient(idClient)).thenReturn(true);
+        when(orderPersistencePort.findByIdClient(idClient)).thenReturn(orderEntity);
         doNothing().when(orderPersistencePort).saveOrder(order);
         TokenInterceptor.setIdUser(2L);
         // Act
         orderUseCase.saveOrder(idRestaurant, order);
+
 
         // Assert
         verify(restaurantPersistencePort, times(1)).findById(idRestaurant);
@@ -57,7 +67,53 @@ class OrderUseCaseTest {
         assertEquals(LocalDate.now(), order.getDate());
         assertEquals(StateEnum.EARNING, order.getState());
         assertEquals(restaurant, order.getRestaurant());
-        verify(orderPersistencePort, times(1)).saveOrder(order);
+        verify(orderPersistencePort, times(2)).saveOrder(order);
+    }
+
+    @Test
+    void testSaveOrderStateDelivered() {
+        // Arrange
+        Long idRestaurant = 1L;
+        Long idClient = 2L;
+        Order order = new Order();
+        Restaurant restaurant = new Restaurant();
+        restaurant.setId(idRestaurant);
+        order.setIdClient(idClient);
+        order.setDate(LocalDate.now());
+        order.setState(StateEnum.EARNING);
+        order.setRestaurant(restaurant);
+        RestaurantEntity restaurantEntity = new RestaurantEntity();
+        restaurantEntity.setId(idRestaurant);
+        OrderEntity orderEntity = new OrderEntity(1L,idClient,LocalDate.now(),StateEnum.DELIVERED,10L,restaurantEntity);
+
+        when(restaurantPersistencePort.findById(idRestaurant)).thenReturn(restaurant);
+        when(orderPersistencePort.existsByIdClient(idClient)).thenReturn(true);
+        when(orderPersistencePort.findByIdClient(idClient)).thenReturn(orderEntity);
+        doNothing().when(orderPersistencePort).saveOrder(order);
+        TokenInterceptor.setIdUser(2L);
+        // Act
+        orderUseCase.saveOrder(idRestaurant, order);
+
+
+        // Assert
+        verify(restaurantPersistencePort, times(1)).findById(idRestaurant);
+        assertEquals(idClient, order.getIdClient());
+        assertEquals(LocalDate.now(), order.getDate());
+        assertEquals(StateEnum.EARNING, order.getState());
+        assertEquals(restaurant, order.getRestaurant());
+        verify(orderPersistencePort, times(2)).saveOrder(order);
+    }
+
+    @Test
+    void testValidateStatePreparation() {
+        // Arrange
+        OrderEntity orderBD = new OrderEntity();
+        orderBD.setState(StateEnum.PREPARATION);
+        Order order = new Order();
+        Restaurant restaurant = new Restaurant();
+
+        // Act and Assert
+        assertThrows(OrderInProcessesException.class, () -> orderUseCase.validateState(orderBD, order, restaurant));
     }
 
     @Test
