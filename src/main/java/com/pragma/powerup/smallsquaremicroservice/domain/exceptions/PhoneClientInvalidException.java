9746606277f71a1d896be132decdae65ecbe3a41
LilianaGallego <lilianagallegom@gmail.com>
2023-06-15 18:44:40 -0500

    build: "validation Phone client"

---
+++ src/main/java/com/pragma/powerup/smallsquaremicroservice/domain/exceptions/PhoneClientInvalidException.java
@@ -0,0 +1,7 @@
+package com.pragma.powerup.smallsquaremicroservice.domain.exceptions;
+
+public class PhoneClientInvalidException extends RuntimeException {
+    public PhoneClientInvalidException() {
+        super();
+    }
+}
